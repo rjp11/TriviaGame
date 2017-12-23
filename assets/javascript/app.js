7e7477aef1656c7bf7f3f1d67b269654a828d6e1
Ryan Parks <rjp@ryans-mbp.attlocal.net>
2017-12-22 18:12:33 -0600

    fully functioning Northwestern football trivia game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,28 +8,36 @@ var intervalId;
 var selectedOption;
 var questions = ["Who holds the record for career rushing yards?",
     "Who holds the record for career passing yards?",
-    "Who holds the record for career receiving yards?"
+    "Who holds the record for career receiving yards?",
+    "Which coach has the most wins in program history?"
 ];
 var totalQuestions = questions.length;
 var options = [
     ["Darnell Autry", "Damien Anderson", "Justin Jackson", "Tyrell Sutton"],
     ["Zak Kustok", "CJ Bacher", "Steve Schnur", "Brett Basanez"],
-    ["Ross Lane", "D'Wayne Bates", "Jeremy Ebert", "Eric Peterman"]
+    ["Ross Lane", "D'Wayne Bates", "Jeremy Ebert", "Eric Peterman"],
+    ["Pat Fitzgerald", "Gary Barnett", "Ara Parseghian", "Randy Walker"]
 ];
 var answers = ["Justin Jackson",
     "Brett Basanez",
-    "D'Wayne Bates"
+    "D'Wayne Bates",
+    "Pat Fitzgerald"
+];
+var images = ["justinjackson.jpg",
+"brettbasanez.jpg",
+"dwaynebates.jpeg",
+"patfitz.jpg"
 ];
 
 //define all functions
-function startGame() {
+function startTrivia() {
     rightAnswer = 0;
     wrongAnswer = 0;
     noAnswer = 0;
     questionNumber = 0;
     time = 30;
     $(".timer").empty();
-    $(".choices").html(`<a class='btn-lg btn-default startGame'>Click to get started!</a>`)
+    $(".question").html(`<br><br><a class='btn-lg btn-default startTrivia'>Click to get started!</a>`)
 };
 
 function reset() {
@@ -39,68 +47,81 @@ function reset() {
     questionNumber = 0;
     time = 30;
     $(".timer").empty();
+    printQuestion();
+};
+
+function clearDivs() {
+    $(".timer").empty();
+    $(".question").empty();
+    $(".choices").empty();
+    $(".option").empty();
+    $(".timer").empty();
+    $(".img").empty();
+    $(".line").empty();
 };
 
 function run() {
     $(".timer").html(`<h2>Time Remaining: <span class="countdown">30</span> Seconds</h2>`);
-    $(".choices").html('');
     intervalId = setInterval(decrement, 1000);
+
+    function decrement() {
+        time--;
+        $(".countdown").text(time);
+        if (time === 0) {
+            clearInterval(intervalId);
+            timesUp();
+        };
+    };
 };
 
-function stop() {
-    clearInterval(intervalId);
-};
-
-function decrement() {
-    //  Decrease time by one.
-    time--;
-    //  Show the number in the #show-number tag.
-    $(".countdown").text(time);
-    if (time === 0) {
-        //  ...run the stop function.
-        stop();
-        //  Alert the user that time is up.
-        alert("Time Up!");
-    }
-};
-
-//functions to write
-//print questions and answers to HTML screen
+//print questions and options to HTML screen
 function printQuestion() {
-    $(".question").text(questions[questionNumber]);
-    $(".a").text(options[questionNumer][0]);
-    $(".b").text(options[questionNumer][1]);
-    $(".c").text(options[questionNumer][2]);
-    $(".d").text(options[questionNumer][3]);
+    clearDivs();
+    $(".question").html("<h2>" + questions[questionNumber] + "</h2>");
+    $("#a").html("<h3>" + options[questionNumber][0] + "</h3>");
+    $("#b").html("<h3>" + options[questionNumber][1] + "</h3>");
+    $("#c").html("<h3>" + options[questionNumber][2] + "</h3>");
+    $("#d").html("<h3>" + options[questionNumber][3] + "</h3>");
+    run();
 };
 
-
-// tally right guess, pause and show right answer, or end game
+// tally right guess, pause and show right answer
 function right() {
     rightAnswer++;
-    $(".question").text(`Go U! The answer is ${answers[questionNumber]}`);
-    setTimeout(pause, 5000);
+    $(".timer").html(`<h3>Go U! The answer was</h3>`);
+    $(".question").html(`<h3>${answers[questionNumber]}</h3>`);
+    $(".img").html("<img class='center-block' src='../TriviaGame/assets/images/" + images[questionNumber] + "'>");
+    console.log(images[questionNumber]);
+    setTimeout(pause, 3000);
 };
 
-// tally wrong guess, pause and show right answer, or end game
+// tally wrong guess, pause and show right answer
 function wrong() {
     wrongAnswer++;
-    $(".question").text(`Nope! The answer is ${answers[questionNumber]}`);
-    setTimeout(pause, 5000);
+    $(".timer").html(`<h3>Nope! The answer is</h3>`);
+    $(".question").html(`<h3>${answers[questionNumber]}</h3>`);
+    $(".img").html("<img class='center-block' src='../TriviaGame/assets/images/" + images[questionNumber] + "'>");
+    setTimeout(pause, 3000);
 };
 
-//3. tally time runs out, pause and show right answer, or end game
+//3. tally "time ups", pause and show right answer
 function timesUp() {
+    clearDivs();
     noAnswer++;
-    $(".question").text(`Time's up! The answer is ${answers[questionNumber]}`);
-    setTimeout(pause, 5000);
+    $(".timer").html(`<h3>Time's up! The answer is</h3>`);
+    $(".question").html(`<h3>${answers[questionNumber]}</h3>`);
+    $(".img").html("<img class='center-block' src='../TriviaGame/assets/images/" + images[questionNumber] + "'>");
+    setTimeout(pause, 3000);
 };
 
 //4. pause on right/wrong/unanswered page, or end game if at end of questions
 function pause() {
-    if (questionNumber < totalQuestions) {
+    if (questionNumber < (totalQuestions - 1)) {
         questionNumber++;
         time = 30;
+        clearDivs();
+        clearInterval(intervalId);
+        printQuestion();
     } else {
         endGame();
     }
@@ -108,22 +129,49 @@ function pause() {
 
 //5. game ends
 function endGame() {
+    clearInterval(intervalId);
+    clearDivs();
     if (rightAnswer === totalQuestions) {
-        $(".question").text(`You know your Wildcats!`)
+        $(".question").html(`<h2>You know your Wildcats!</h2>`)
     } else if (rightAnswer > 0) {
-        $(".question").text(`You did alright, but there is room for improvement!`)
+        $(".question").html(`<h2>You did alright, but there is room for improvement!</h2>`)
     } else {
-        $(".question").text("Well, you can't do any worse next time...")
+        $(".question").html(`<h2>Well, you can't do any worse next time...</h2>`)
     };
-    $(".a").text(`Correct Answers: ${rightAnswer}`);
-    $(".b").text(`Wrong Answers: ${wrongAnswer}`);
-    $(".c").text(`Unanswered: ${noAnswer}`);
-
+    $(".one").html(`<h3>Correct Answers: ${rightAnswer}</h3>`);
+    $(".two").html(`<h3>Wrong Answers: ${wrongAnswer}</h3>`);
+    $(".three").html(`<h3>Unanswered: ${noAnswer}</h3>`);
+    $(".four").html(`<button class = "reset btn-lg btn-default">Try Again</button>`)
 };
 
 //now, run all of these damn functions you wrote
+$(document).ready(function () {
 
-//if selection === answer, run right function 
-//if selection !=  answer, run wrongs function
+    startTrivia();
+
+    $("body").on("click", ".startTrivia", function (event) {
+        printQuestion();
+    });
+
+    $("body").on("click", ".option", function (event) {
+        selectedOption = $(this).text();
+        clearDivs();
+        clearInterval(time);
+        //if selection === answer, run right function 
+        if (selectedOption === answers[questionNumber]) {
+            right();
+        }
+        //if selection !=  answer, run wrongs function
+        else {
+            wrong();
+        }
+    });
+
+    $("body").on("click", ".reset", function(event){
+        reset();
+    });
+});
+
+
 //if time === 0, run timesUp function
 //when questionNumber = totalQuestions, end game and return results
